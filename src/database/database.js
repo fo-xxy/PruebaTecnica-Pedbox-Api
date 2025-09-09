ed713df4583e25d20dd09d6b1bdb2433d5e12835
fo-xxy <robert.yarce.12@hotmail.com>
2025-09-09 08:48:24 -0500

    Variables de la base de datos cambiadas a las del proyecto

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2/promise');
 
-/*const dbConfig = {
+const dbConfig = {
     host: 'localhost',
     user: 'root',        
     password: '001001', 
@@ -8,18 +8,8 @@ const mysql = require('mysql2/promise');
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
-};*/
-
-const dbConfig = {
-  host: process.env.MYSQLHOST || 'localhost',
-  user: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || '001001',
-  database: process.env.MYSQLDATABASE || 'pedbox_temas',
-  port: process.env.MYSQLPORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 };
+
 
 const createConnection = async () => {
     
